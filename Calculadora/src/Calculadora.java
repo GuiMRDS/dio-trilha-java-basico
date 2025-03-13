a3b265edff70266656a2db2bf49b5b825dca460e
Guilherme Marinho <guimars22@gmail.com>
2025-03-13 17:27:11 -0300

    #

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,11 @@
+/* 
+ * @author Gulherme Marinho 
+ * @Version 1.0
+ * @ 01/01/2022
+*/
+
+public class Calculadora {
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
