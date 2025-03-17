a43039b5f47e40854e15f4042ebb857e05e0f80a
Guilherme Marinho <guimars22@gmail.com>
2025-03-17 19:38:10 -0300

    Exemplos de laços de repetição

---
+++ Nova pasta/controle-fluxo/src/swichCase.java
@@ -0,0 +1,21 @@
+public class swichCase {
+    public static void main(String[] args) {
+        String sigla = "A";
+
+        switch (sigla) {
+            case "P":
+                System.out.println("PEQUENO");
+                break;
+            case "M":
+                System.out.println("MEDIA");
+                break;
+            case "G":
+                System.out.println("GRANDE");
+                break;
+        
+            default:
+                System.out.println("INDEFINIDO");
+                break;
+        }
+    }
+}
